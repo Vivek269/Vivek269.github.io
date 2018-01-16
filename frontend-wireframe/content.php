66a4ed1887d402aa0ecfcc8627eadbf4d2f23f43
vivek <viveksinha@github.com>
2018-01-16 22:09:34 +0530

    added proj files..
    please click on repository link..index file will rediect to diff domain

---
+++ frontend-wireframe/content.php
@@ -0,0 +1,8 @@
+<div class=" background" id="background1">
+ </div><!--close for text-wrap-1-->
+<div class="background" id="background2">
+</div><!--close for text-wrap-2-->
+<div class="text-wrap3  background" id="background3">
+</div><!--close for text-wrap3-->   
+<div class="background" id="background4">
+</div><!--close for text-wrap4--> 
